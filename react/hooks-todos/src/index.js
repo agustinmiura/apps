544aa02b5926c5ba47355f2fce29b2c23d616250
A.R.M <miura.agustin@protonmail.com>
2021-05-25 21:57:25 -0300

    Todos app initial structure

--- react/hooks-todos/src/index.js
+++ react/hooks-todos/src/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-export const UserContext = React.createContext();
+import TodosContext from './context';
+import todosReducer from './reducer';
 
-const username = "Dave";
+import TodoList from './components/TodoList';
+
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
+
+  return (
+    <TodosContext.Provider value={{state, dispatch}}>
+      <TodoList />
+    </TodosContext.Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContext.Provider value={username}>
-      <App/>
-    </UserContext.Provider>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
