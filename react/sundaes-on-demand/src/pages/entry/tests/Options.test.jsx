d16cbcb75458a88e1c1dae759fb3546bcf9b5b45
agustinmiura <miura.agustin@protonmail.com>
2021-05-30 03:49:56 -0300

    Fixed tests

--- react/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
+++ react/sundaes-on-demand/src/pages/entry/tests/Options.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { scryRenderedComponentsWithType } from "react-dom/test-utils";
 
 import Options from "../Options";
 
@@ -14,4 +15,19 @@ test("displays image for each scoop", async () => {
   const altText = scoopTimes.map((element) => element.alt);
 
   expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("Displays image for each toppings option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole("img", { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const imageTitles = images.map((img) => img.alt);
+
+  expect(imageTitles).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
 });
