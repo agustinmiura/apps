804d3322a69a6f6062b8a077ecd40c24e482e975
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2021-06-03 02:51:14 +0000

    Feature/spring boot mongo kotlin

--- kotlin/mongo-db-api/src/main/kotlin/ar/com/miura/mongodbapi/repository/StudentRepository.kt
+++ kotlin/mongo-db-api/src/main/kotlin/ar/com/miura/mongodbapi/repository/StudentRepository.kt
@@ -2,11 +2,15 @@ package ar.com.miura.mongodbapi.repository
 
 import ar.com.miura.mongodbapi.domain.Student
 import org.springframework.data.mongodb.repository.MongoRepository
+<<<<<<< HEAD
 import org.springframework.data.mongodb.repository.Query
+=======
+>>>>>>> 1125c07 (Feature/spring boot mongo kotlin)
 import org.springframework.stereotype.Repository
 
 @Repository
 interface StudentRepository : MongoRepository<Student, String> {
+<<<<<<< HEAD
 
     fun findByName(name:String):List<Student>
 
@@ -26,4 +30,6 @@ interface StudentRepository : MongoRepository<Student, String> {
 
     @Query("{\"name\":\"?0\"}")
     fun findByNameWithCustomQuery(name:String):List<Student>
+=======
+>>>>>>> 1125c07 (Feature/spring boot mongo kotlin)
 }
