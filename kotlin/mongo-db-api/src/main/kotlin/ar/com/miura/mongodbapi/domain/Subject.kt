d8cf633671f48795bd3b3a06381c061739c880ad
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2021-06-05 04:03:00 +0000

    Added queries and endpoints .

--- kotlin/mongo-db-api/src/main/kotlin/ar/com/miura/mongodbapi/domain/Subject.kt
+++ kotlin/mongo-db-api/src/main/kotlin/ar/com/miura/mongodbapi/domain/Subject.kt
@@ -1,6 +1,7 @@
 package ar.com.miura.mongodbapi.domain
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
@@ -13,12 +14,24 @@ class Subject(
     @Field("subject_name")
     val subjectName: String?="",
 =======
+=======
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+>>>>>>> d8a8e34 (Added queries and endpoints .)
 import org.springframework.data.mongodb.core.mapping.Field
 
+@Document(collection="subject")
 class Subject(
+    @Id
+    val id: ObjectId = ObjectId.get(),
     @Field("subject_name")
+<<<<<<< HEAD
     val name: String?="",
 >>>>>>> 1125c07 (Feature/spring boot mongo kotlin)
+=======
+    val subjectName: String?="",
+>>>>>>> d8a8e34 (Added queries and endpoints .)
     @Field("marks_obtained")
     val marksObtained: Int =Integer.valueOf("0")
 ) {
