de4b3121cd59e312be2de8c8e1636cb6ebb37c5b
agustinmiura <miura.agustin@protonmail.com>
2021-06-09 00:51:26 -0300

    Added posibility to customized secuirity

--- kotlin/bank-api/src/main/kotlin/ar/com/miura/api/config/SecurityConfig.kt
+++ kotlin/bank-api/src/main/kotlin/ar/com/miura/api/config/SecurityConfig.kt
@@ -1,24 +1,33 @@
 package ar.com.miura.api.config
 
+import ar.com.miura.api.enum.SecurityConfigEnum
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 open class SecurityConfig(disableDefaults: Boolean = false) : WebSecurityConfigurerAdapter(disableDefaults) {
-
+    val state: SecurityConfigEnum = SecurityConfigEnum.ALLOW_ALL
     @Override
     override fun configure(http:HttpSecurity) {
-        http.authorizeRequests()
-            .antMatchers("/myAccount").authenticated()
-            .antMatchers("/myBalance").authenticated()
-            .antMatchers("/myLoans").authenticated()
-            .antMatchers("/myCards").authenticated()
-            .antMatchers("/notices").permitAll()
-            .antMatchers("/contact").permitAll()
-            .and()
-            .formLogin().and()
-            .httpBasic()
+        if (state==SecurityConfigEnum.CUSTOM) {
+            http.authorizeRequests()
+                .antMatchers("/myAccount").authenticated()
+                .antMatchers("/myBalance").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .and()
+                .formLogin().and()
+                .httpBasic()
+        } else if (state==SecurityConfigEnum.DENY_ALL) {
+            http.authorizeRequests().anyRequest().denyAll().and()
+                .formLogin().and().httpBasic()
+        } else {
+            http.authorizeRequests().anyRequest().permitAll().and()
+                .formLogin().and().httpBasic()
+        }
     }
 
 }
