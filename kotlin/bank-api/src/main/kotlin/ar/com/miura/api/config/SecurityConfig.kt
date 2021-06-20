75427a7e12d8290ee07d0f4bdc1c43a309120d7e
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2021-06-20 00:52:36 +0000

    feature/security

--- kotlin/bank-api/src/main/kotlin/ar/com/miura/api/config/SecurityConfig.kt
+++ kotlin/bank-api/src/main/kotlin/ar/com/miura/api/config/SecurityConfig.kt
@@ -1,15 +1,22 @@
 package ar.com.miura.api.config
 
 import ar.com.miura.api.enum.SecurityConfigEnum
+import ar.com.miura.api.enum.UserDetailsEnum
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
 open class SecurityConfig(disableDefaults: Boolean = false) : WebSecurityConfigurerAdapter(disableDefaults) {
 
     val securityConfig: SecurityConfigEnum = SecurityConfigEnum.CUSTOM
+
+    val userDetailsConfig: UserDetailsEnum = UserDetailsEnum.MEMORY
 
     @Override
     override fun configure(http:HttpSecurity) {
@@ -35,10 +42,25 @@ open class SecurityConfig(disableDefaults: Boolean = false) : WebSecurityConfigu
 
     @Override
     override fun configure(auth:AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin")
-        .and()
-        .withUser("user").password("12345").authorities("read")
-        .and().passwordEncoder((PlainTextPasswordEncoder()))
+        if (userDetailsConfig==UserDetailsEnum.MEMORY) {
+            val userDetailsService = InMemoryUserDetailsManager()
+            val user = User.withUsername("admin").password("12345").authorities("admin").build()
+            val user1 = User.withUsername("user").password("12345").authorities("read").build()
+            userDetailsService.createUser(user)
+            userDetailsService.createUser(user1)
+            auth.userDetailsService(userDetailsService)
+
+        } else {
+            auth.inMemoryAuthentication().withUser("admin").password("12345").authorities("admin")
+                .and()
+                .withUser("user").password("12345").authorities("read")
+                .and().passwordEncoder((PlainTextPasswordEncoder()))
+        }
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return PlainTextPasswordEncoder()
     }
 
 }
