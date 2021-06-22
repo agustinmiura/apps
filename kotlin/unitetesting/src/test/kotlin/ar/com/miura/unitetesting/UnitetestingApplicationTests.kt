4208aa7c058b258113d01bf016d4c2053ba31db7
A R M <1576386-agustinmiura@users.noreply.gitlab.com>
2021-06-22 03:42:36 +0000

    feature/mock Created proyect and mock tests.

---
+++ kotlin/unitetesting/src/test/kotlin/ar/com/miura/unitetesting/UnitetestingApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.com.miura.unitetesting
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnitetestingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
